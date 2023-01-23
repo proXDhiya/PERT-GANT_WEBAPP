f8c10663eee09d31429ad3898799247fe15d7d3a
proXDhiya <prox.dhiya@gmail.com>
2023-01-23 21:39:23 +0100

    Update

--- public/js/projectGantt.js
+++ public/js/projectGantt.js
@@ -1,13 +1,6 @@
 anychart.onDocumentReady(() => {
     // Data for gantt chart
-    var data = [
-        {id: "1", name: "Task A", start: "2019-01-01", end: "2019-01-02"},
-        {id: "2", name: "Task B", start: "2019-01-02", end: "2019-01-06"},
-        {id: "3", name: "Task C", start: "2019-01-03", end: "2019-01-05"},
-        {id: "4", name: "Task D", start: "2019-01-04", end: "2019-01-05"},
-        {id: "5", name: "Task AD", start: "2019-01-05", end: "2019-01-06", dependsOn: ["1", "4"]},
-        {id: "6", name: "Task BC", start: "2019-01-06", end: "2019-01-07", dependsOn: ["2", "3"]}
-    ];
+    var data = JSON.parse(document.querySelector('.table__save').value);
 
     // tree data
     var treeData = anychart.data.tree(data, "asTable");
